2c74cc6e1dbf4626c90c09e9c303ed9e794b1b5c
MyeongSic <you6878@icloud.com>
2017-08-01 20:32:03 +0900

    Initial Commit

---
+++ HowlTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowlTalk
+//
+//  Created by 유명식 on 2017. 8. 1..
+//  Copyright © 2017년 swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
